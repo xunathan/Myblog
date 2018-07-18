e9b225a3dff77d5cc3c5681f36922dc45516a6e9
xunathan <xwcjdn@gmail.com>
2018-07-18 21:50:41 +0800

    add index html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+#-*- coding:utf-8 -*-
 from django.shortcuts import render
-
+from django import template
 # Create your views here.
+
+class IndexView(ListView):
+	template_name = 'blog/index.html'
+
+	def get_context_data(self,**kwargs):
+		kwargs['website_title'] = "My Test Blog"
+		return super(IndexView,self).get_context_data(**kwargs)
+
+		
